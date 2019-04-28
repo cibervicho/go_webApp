8faec646a45b2c8392fb60a6b3dbc20b26a27b13
David M. Moreno <david.mmoreno@gmail.com>
2019-04-28 01:11:20 -0500

    Adding a capability to accept a URL from command line in order to connect to a remotely mongoDB

--- config/db.go
+++ config/db.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"github.com/globalsign/mgo"
 	"log"
+	"os"
 )
 
 const (
-	url = "localhost:27017"
+	//url = "localhost:27017"
+	//url = "mongodb://192.168.1.66:27017/moviesdb"}
+	//url = "laptop-03garp8v"
 	my_db = "moviesdb"
 	my_coll = "imdb"
 )
+
+var url string
 
 // database
 var DB *mgo.Database
@@ -19,6 +24,15 @@ var DB *mgo.Database
 var IMBD *mgo.Collection
 
 func init() {
+	if 1 == len(os.Args) {
+		url = "localhost:27017"
+		log.Printf("No db address specified, using '%v' for now", url)
+	} else {
+		// set the specified mongodb database in command line
+		url = os.Args[1]
+		log.Printf("Using the IP '%v' as the db address", url)
+	}
+	
 	// connecting to the mongodb server
 	session, err := mgo.Dial(url)
 	if err != nil {
