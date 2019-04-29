41fe701e4c95dd21c6bfb24b9994c7ade94c466a
David M. Moreno <david.mmoreno@gmail.com>
2019-04-29 01:03:12 -0500

    Since this app will be running in the same server as where it is running Jenkins, and because the latter is already using the 8080 port I changed this app's listening port to 8008 instead.

--- main.go
+++ main.go
@@ -3,24 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+
 	"github.com/cibervicho/go_webApp/movies"
 )
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/movies", movies.Index)
-	
+
 	http.HandleFunc("/movies/show", movies.Show)
-	
+
 	http.HandleFunc("/movies/create", movies.Create)
 	http.HandleFunc("/movies/create/process", movies.CreateProcess)
-	
+
 	http.HandleFunc("/movies/update", movies.Update)
 	http.HandleFunc("/movies/update/process", movies.UpdateProcess)
-	
+
 	http.HandleFunc("/movies/delete/process", movies.DeleteProcess)
-	
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	log.Fatal(http.ListenAndServe(":8008", nil))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
